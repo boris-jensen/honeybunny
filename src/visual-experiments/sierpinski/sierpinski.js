9e2b552cdbc8c5e260ff9c1eff2133a0df1ac943
Boris Søborg Jensen <boris.soborgjensen@crowdstrike.com>
2022-12-30 17:24:40 +0100

    colours for corners

--- src/visual-experiments/sierpinski/sierpinski.js
+++ src/visual-experiments/sierpinski/sierpinski.js
@@ -1,53 +1,16 @@
-
-function byteToHex(byte) {
-  if (byte < 16) {
-    return '0' + byte.toString(16)
-  } else {
-    return byte.toString(16)
-  }
-}
-  
-function toColor(color) {
-  const colorShare = 255 * (color / 100)
-  const red = Math.round(255 - colorShare)
-  const green = Math.round(colorShare)
-
-  return '#' + byteToHex(red) + byteToHex(green) + '00'
-}
-  
-class Point {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-  }
-}
-
-function newPoint(from, angle, length) {
-  const deltaX = Math.cos(angle) * length
-  const deltaY = Math.sin(angle) * length
-  return new Point(from.x + deltaX, from.y + deltaY)
-}
-  
-function line(from, angle, length, ctx) {
-  const to = newPoint(from, angle, length)
-  ctx.lineTo(to.x, to.y)
-  return to
-}
-
-function reset() {
-  resetConfig()
-}
-
-const corners = [
-  new Point(100, 500),
-  new Point(600, 700),
-  new Point(200, 100),
-]
-
 function drawPoint(point, ctx) {
   const x = Math.round(point.x)
   const y = Math.round(point.y)
   ctx.fillRect(x, y, 1, 1)
+}
+
+function drawCorner(point, ctx) {
+  const x = Math.round(point.x)
+  const y = Math.round(point.y)
+  const side = 6
+  const halfSide = side / 2
+
+  ctx.fillRect(x - halfSide, y - halfSide, side, side)
 }
 
 function midwayPoint(p1, p2) {
@@ -59,7 +22,9 @@ function midwayPoint(p1, p2) {
 
 function selectCorner() {
   const idx = Math.floor(Math.random() * 3)
-  return corners[idx]
+  if (idx == 0) return config.corners.pointA
+  else if (idx == 1) return config.corners.pointB
+  else return config.corners.pointC
 }
 
 function paintSierpinski() {
@@ -67,11 +32,15 @@ function paintSierpinski() {
   const ctx = canvas.getContext('2d')
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath()
-  ctx.strokeStyle = toColor(getColor())
-  for (let i = 0; i < 3; i++) {
-    drawPoint(corners[i], ctx)
-  }
 
+  ctx.fillStyle = "#FF0000"
+  console.log(ctx.strokeStyle)
+  drawCorner(config.corners.pointA, ctx)
+  drawCorner(config.corners.pointB, ctx)
+  drawCorner(config.corners.pointC, ctx)
+  ctx.stroke()
+
+  ctx.fillStyle = "#000000"
   let current = selectCorner()
   for (let i = 0; i < getDots(); i++) {
     current = midwayPoint(current, selectCorner())
--- src/visual-experiments/sierpinski/sierpinskiConfig.js
+++ src/visual-experiments/sierpinski/sierpinskiConfig.js
@@ -1,54 +1,65 @@
 const LIMITS = {
-  DOTS:  { MAX: 10000, MIN: 1 },
-  SPEED: { MAX: 100,  MIN: 1 },
-  COLOR: { MAX: 100,  MIN: 0 },
+  DOTS:    { MAX: 10000, MIN: 1 },
+  SPEED:   { MAX: 100,   MIN: 1 },
+  COLOR:   { MAX: 100,   MIN: 0 },
 }
-  
+
 const DEFAULTS = {
   DOTS: 100,
   SPEED: 50,
   COLOR: 0
 }
-  
+
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
 function initialConfig() {
   return {
     dots: DEFAULTS.DOTS,
     speed: DEFAULTS.SPEED,
     color: DEFAULTS.COLOR,
-  }
-}
-  
-  let config = initialConfig()
-  
-  function resetConfig() {
-    config = initialConfig()
-  }
-  
-  function getDots() { return config.dots }
-  function getSpeed() { return config.speed }
-  function getColor() { return config.color}
-  
-  function hookupConfig() {
-    hookupConfigElement("dots-input", "dots-value", "dots", "DOTS")
-    hookupConfigElement("speed-input", "speed-value", "speed", "SPEED")
-    hookupConfigElement("color-input", "color-value", "color", "COLOR")
-  }
-  
-  function hookupConfigElement(inputId, valueId, configField, limitsField) {
-    const input = document.getElementById(inputId)
-    const value = document.getElementById(valueId)
-    const initialValue = config[configField]
-    const inputLimits = LIMITS[limitsField]
-  
-    input.setAttribute("min", inputLimits.MIN)
-    input.setAttribute("max", inputLimits.MAX)
-    input.value = initialValue
-    value.innerHTML = initialValue
-    input.oninput = function() {
-      value.innerHTML = this.value
-      console.log(this.value)
-      config[configField] = this.value
-      console.log(config)
-      paintSierpinski()
+    corners: {
+      pointA: new Point(500, 50),
+      pointB: new Point(50, 800),
+      pointC: new Point(950, 800),
     }
   }
+}
+
+let config = initialConfig()
+
+function getDots() { return config.dots }
+function getSpeed() { return config.speed }
+function getColor() { return config.color}
+
+function hookupConfig() {
+  hookupConfigElement("dots-input", "dots-value", "dots", "DOTS")
+  hookupConfigElement("speed-input", "speed-value", "speed", "SPEED")
+  hookupConfigElement("color-input", "color-value", "color", "COLOR")
+  hookupCorners()
+}
+
+function hookupConfigElement(inputId, valueId, configField, limitsField) {
+  const input = document.getElementById(inputId)
+  const value = document.getElementById(valueId)
+  const initialValue = config[configField]
+  const inputLimits = LIMITS[limitsField]
+
+  input.setAttribute("min", inputLimits.MIN)
+  input.setAttribute("max", inputLimits.MAX)
+  input.value = initialValue
+  value.innerHTML = initialValue
+  input.oninput = function() {
+    value.innerHTML = this.value
+    config[configField] = this.value
+    paintSierpinski()
+  }
+}
+
+function hookupCorners() {
+  
+}
